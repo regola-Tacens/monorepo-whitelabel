2df6a572907fd01bf1e4ac5145732a5066cd106f
regola-Tacens <regolas-tacens@outlook.fr>
2022-10-22 09:24:45 +0100

    init

--- apps/aftersale/next.config.js
+++ apps/aftersale/next.config.js
@@ -7,7 +7,7 @@ const { withNx } = require('@nrwl/next/plugins/with-nx');
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  basePath: '/smart-repair',
+  basePath: '/aftersale',
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
